7ff152f2dd6a179ea436fd28139cb1e1017ba9a7
spkaluzny <spkaluzny@gmail.com>
2023-04-27 20:00:22 -0700

    Initial setup

---
+++ scripts/combineBirdsSeen.R
@@ -0,0 +1,13 @@
+DATADIR <- here::here("data")
+files_by_year <- list.files(DATADIR, pattern="birdsSeen_.*txt")
+birds_by_year <- NULL
+for(f in files_by_year) {
+  b <- read.table(file.path(DATADIR, f), sep=",", quote="")
+  names(b) <- c("Species", "LeadersOnly")
+  date <- as.Date(f, format="birdsSeen_%Y%m%d.txt")
+  year <- lubridate::year(date)
+  d <- cbind(b, Year=rep(year, nrow(b)))
+  birds_by_year <- rbind(birds_by_year, d)
+}
+write.table(birds_by_year, file=file.path(DATADIR, "BirdsSeenByYear.csv"),
+  sep=",", row.names=FALSE)
