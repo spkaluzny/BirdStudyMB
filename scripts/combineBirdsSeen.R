c5f6d5289019c88af7a422a7a4097ba2599339ef
spkaluzny <spkaluzny@gmail.com>
2023-05-09 18:43:10 -0700

    Changed LeadersOnly to SeenBy

--- scripts/combineBirdsSeen.R
+++ scripts/combineBirdsSeen.R
@@ -3,7 +3,7 @@ files_by_year <- list.files(DATADIR, pattern="birdsSeen_.*txt")
 birds_by_year <- NULL
 for(f in files_by_year) {
   b <- read.table(file.path(DATADIR, f), sep=",", quote="")
-  names(b) <- c("Species", "LeadersOnly")
+  names(b) <- c("Species", "SeenBy")
   date <- as.Date(f, format="birdsSeen_%Y%m%d.txt")
   year <- lubridate::year(date)
   d <- cbind(b, Year=rep(year, nrow(b)))
